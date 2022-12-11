3ffbe8f07feff4c088978cbb3aac87b752b91aa7
Roman_Chub <rchub@inbox.ru>
2022-12-12 00:22:37 +0300

    Transportation on vacation
    is done

---
+++ Transportation_on_vacation/Solution.js
@@ -0,0 +1,14 @@
+function CarRental(d) {
+    let eachDayRent = 40;
+    let res;
+    if (d >= 7) {
+        res = (d * eachDayRent) - 50;
+    } else if (d >= 3 && d <= 7) {
+        res = (d * eachDayRent) - 20;
+    } else if (d < 3) {
+        res = d * eachDayRent;
+    }
+    return res;
+}
+
+console.log(CarRental(2))
