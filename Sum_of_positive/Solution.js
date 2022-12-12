67954e5540df8e1e1956f35911b01a0e9f14fb28
Roman_Chub <rchub@inbox.ru>
2022-12-12 22:46:38 +0300

    Sum of positive is done

---
+++ Sum_of_positive/Solution.js
@@ -0,0 +1,12 @@
+let arr = [-1,2,3,4,-5]
+function positiveSum(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) sum += arr[i]
+    }
+    return sum
+}
+console.log(positiveSum(arr))
+
+
+
