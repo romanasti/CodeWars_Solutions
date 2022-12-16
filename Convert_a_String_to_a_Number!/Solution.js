fb205af982e9676edea0b329003b89001080111e
Roman_Chub <rchub@inbox.ru>
2022-12-17 01:01:14 +0300

    Convert a String to a Number! is done

---
+++ Convert_a_String_to_a_Number!/Solution.js
@@ -0,0 +1,20 @@
+const stringToNumber = function (str) {
+    return Number(str)
+}
+
+// any mathematical action
+//  Number,
+// 	parseInt,
+// 	parseFloat,
+//
+// 	s => Number(s),
+// 	s => parseInt(s),
+// 	s => parseFloat(s),
+//
+// 	s => Math.floor(s),
+// 	s => Math.round(s),
+// 	s => Math.ceil(s),
+// 	s => Math.min(s),
+// 	s => Math.max(s),
+
+console.log(stringToNumber("1234"))
