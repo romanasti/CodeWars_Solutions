dc96b0cca1fbbb768481bd8a1d687df4e22a4c53
Roman_Chub <rchub@inbox.ru>
2022-12-30 23:01:50 +0300

    Find the smallest integer in the array is done

---
+++ 8 kyu/Find_the_smallest_integer_in_the_array/Solution.js
@@ -0,0 +1,12 @@
+function findSmallestInt(args) {
+    let smallest = args[0]
+    for (let i = 0; i < args.length; i++) {
+        if(args[i] < smallest) {
+            smallest = args[i]
+        }
+    }
+    return smallest
+}
+
+console.log(findSmallestInt([34, -345, -1, 100]))
+
