c25cba7f068129e46910670bc0645517f1ce67ef
Roman_Chub <rchub@inbox.ru>
2023-01-08 01:10:07 +0300

    Split Strings is done

---
+++ 6 kyu/Split_Strings/Solution.js
@@ -0,0 +1,12 @@
+function solution(str) {
+    let result = [];
+    if (str.length % 2 !== 0) {
+        str += "_"
+    }
+    for (let i = 0; i < str.length; i+=2) {
+        result.push(str[i] + str[i+1])
+    }
+    return result
+}
+
+console.log(solution("abcdefg"))
